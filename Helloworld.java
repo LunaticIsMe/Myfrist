f87b0b52654a796c4495a819e98e70ff43880ae8
Fadillah Luthfie Naufal <97927018+LunaticIsMe@users.noreply.github.com>
2022-01-18 09:51:46 +0700

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,5 +2,6 @@ public class Helloworld {
   public static void main(){
     System.out.println("Hello Pew");
     System.out.println("Black furqon");
+    System.out.println("amri kita bantai furqon")
   }
 }
